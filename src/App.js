d0022fa5f4f7e58767a5742a67415b7687e5f682
Standard <oanlara8@gmail.com>
2023-01-20 22:46:02 -0700

    basic skeleton structure

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // a variable that will be used in the return statement
+  // it is simply an html that will take up half the page
+  const halfPage = <div className="half-page">hey</div>;
+  const displayRecursion = <div className="half-page">hey</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {halfPage}
+      {displayRecursion}
     </div>
   );
 }
